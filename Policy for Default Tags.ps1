86d9e3ddb21e64fe29ef1729e7a6ff94ca879200
Adam Paternostro <AdamPaternostro@users.noreply.github.com>
2018-01-05 14:52:01 -0500

    Add files via upload

---
+++ Policy for Default Tags.ps1
@@ -0,0 +1,42 @@
+﻿# Create Policy
+
+$subscriptionId = "<<REMOVED>>"
+Login-AzureRmAccount
+
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+$subscriptionScope = "/subscriptions/" + $subscriptionId
+
+$policyName = "Required-Tags"
+$policyDescription = "Tags that are required as part of every resource."
+
+# Only allow LRS and GRS storage accounts.
+$policyDefinition = New-AzureRmPolicyDefinition `
+                        -Name $policyName `
+                        -Description $policyDescription `
+                        -Policy `
+                        '{
+                          "$schema": "http://schema.management.azure.com/schemas/2015-10-01-preview/policyDefinition.json",
+                          "if": {
+                            "exists": false,
+                            "field": "tags"
+                          },
+                          "then": {
+                            "effect": "append",
+                            "details": [
+                              {
+                                "field": "tags",
+                                "value": { "CostCenter": "DEFAULT", "Category": "DEFAULT" }
+                              }
+                            ]
+                          }
+                        }'
+
+
+New-AzureRmPolicyAssignment `
+        -Name $policyName"-Policy-Sub-Scope" `
+        -PolicyDefinition $policyDefinition `
+        -Scope $subscriptionScope
+
+#Remove-AzureRmPolicyAssignment `
+#        -Name $policyName"-Policy-Sub-Scope" `
+#        -Scope $subscriptionScope
