7393ae5ff097888925d20b2c269b25ca77a5a51d
Adam Paternostro <AdamPaternostro@users.noreply.github.com>
2018-01-08 08:44:07 -0500

    Add files via upload

---
+++ Create Default Tags.ps1
@@ -0,0 +1,11 @@
+﻿$subscriptionId = "<<REMOVED>>"
+
+# Connect to Azure
+Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+New-AzureRmTag -Name "Environment" -Value "Dev"
+New-AzureRmTag -Name "Environment" -Value "QA"
+New-AzureRmTag -Name "Environment" -Value "Demo"
+New-AzureRmTag -Name "Environment" -Value "Prod"
+
